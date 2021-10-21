f455a66f790a2a9b5b43787fc992d4084568d374
DmytriyBochkovoy <85780204+DmytriyBochkovoy@users.noreply.github.com>
2021-10-21 20:43:07 +0300

    adaptive

--- gr_1_bochkovoy_dmytriy_dz_9/js/script.js
+++ gr_1_bochkovoy_dmytriy_dz_9/js/script.js
@@ -26,4 +26,33 @@ $('.slider_project').slick({
         }
       }
     ]
-  });
+});
+$('.sliders_activity').slick({
+    dots: true,
+    slidesToShow: 1,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+        {
+            dots: true,
+            breakpoint: 992,
+            settings: {
+            slidesToShow: 1
+            }
+        },
+        {
+            dots: true,
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 1
+        }
+        },
+        {
+            dots: true,
+            breakpoint: 320,
+            settings: {
+            slidesToShow: 1
+        }
+      }
+    ]
+});
