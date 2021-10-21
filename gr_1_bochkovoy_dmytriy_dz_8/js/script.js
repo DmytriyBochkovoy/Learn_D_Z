33043cf9cf12fb456d4a78efad9b63ef7750a3a7
DmytriyBochkovoy <85780204+DmytriyBochkovoy@users.noreply.github.com>
2021-10-22 00:16:48 +0300

    adaptive

--- gr_1_bochkovoy_dmytriy_dz_8/js/script.js
+++ gr_1_bochkovoy_dmytriy_dz_8/js/script.js
@@ -1,5 +1,4 @@
 $('.slider-for').slick({
-    centerPadding: '60px',
     slidesToShow: 3,
     prevArrow: "<img src='img/arrow_left.svg' class='slick-prev' alt='1'>",
     nextArrow: "<img src='img/arrow_right.svg' class='slick-next' alt='2'>",
@@ -8,7 +7,6 @@ $('.slider-for').slick({
             breakpoint: 992,
             settings: {
             arrows: false,
-            centerMode: true,
             centerPadding: '40px',
             slidesToShow: 2
             }
@@ -17,7 +15,6 @@ $('.slider-for').slick({
             breakpoint: 768,
             settings: {
             arrows: false,
-            centerMode: true,
             centerPadding: '40px',
             slidesToShow: 1
         }
@@ -26,7 +23,6 @@ $('.slider-for').slick({
             breakpoint: 320,
             settings: {
             arrows: false,
-            centerMode: true,
             centerPadding: '40px',
             slidesToShow: 1
         }
